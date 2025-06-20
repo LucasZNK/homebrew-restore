417cb56b0457efd622e993e2c54be7cd4aed9248
Lucas Zanek <lucaszanek94@gmail.com>
2025-06-19 21:30:41 -0300

    Update restore to version 1.1.1
    - Updated download URL to v1.1.1
    - Updated SHA256 checksum
    - Added version test to formula

--- restore.rb
+++ restore.rb
@@ -1,8 +1,8 @@
 class Restore < Formula
   desc "Interactive Git save point browser"
   homepage "https://github.com/LucasZNK/restore"
-  url "https://github.com/LucasZNK/restore/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "2ab497ca168e9c68189771189df11bdfdf9458085db5fe24d46154c86127bfad"
+  url "https://github.com/LucasZNK/restore/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "61805022736a6134e3dfd8b31bfc8a52341fb65b31cd5e76e8d674cb8ed390eb"
   license "MIT"
 
   def install
@@ -13,5 +13,8 @@ class Restore < Formula
     # Test that the script exists and is executable
     assert_predicate bin/"restore", :exist?
     assert_predicate bin/"restore", :executable?
+    
+    # Test version flag
+    assert_match "restore version 1.1.1", shell_output("#{bin}/restore --version")
   end
 end
